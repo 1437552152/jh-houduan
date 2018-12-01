f95df1b252fbc31411ef6d2a7e8f331ebd904a2b
yeyifu <1437552152@qq.com>
2018-12-01 08:19:10 +0800

    first commit

---
+++ conf/conf.js
@@ -0,0 +1,18 @@
+
+var mysql = require("mysql");
+var pool = mysql.createPool({
+    host     : '139.196.96.67',
+    user     : 'root',
+    password : 'scpcxb',
+    database : 'jiehang',
+    port:3306,
+});
+function query(sql,values,callback){
+    pool.getConnection(function(err,connection){
+        connection.query(sql,values, function (err,rows) {
+            callback(err,rows);
+            connection.release();
+        });
+    });
+}
+exports.query = query;
---
+++ routes/user.js
@@ -0,0 +1,17 @@
+// var express = require('express');
+// var router = express.Router();
+// var  db = require('../conf/conf');
+// //登录
+// router.post('/login', function(req, res) { 
+//   console.log(req);  
+// });
+
+
+
+
+
+
+
+
+
+module.exports = router;
