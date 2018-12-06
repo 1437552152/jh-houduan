524d45d5ad399ec982fda020e55d3e8407b6d898
yeyifu <1437552152@qq.com>
2018-12-06 17:47:48 +0800

    接口增加

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var Cookies = require('cookies');
 var app = express();
 //设置静态文件托管
 //当用户访问的url以/public开始，那么直接返回对应__dirname + '/public'下的文件
-app.use( '/public', express.static( __dirname + '/public') );
+app.use('/public', express.static(__dirname + '/public'));
 //配置应用模板
 //定义当前应用所使用的模板引擎
 //第一个参数：模板引擎的名称，同时也是模板文件的后缀，第二个参数表示用于解析处理模板内容的方法
@@ -27,18 +27,24 @@ app.set('views', './views');
 //注册所使用的模板引擎，第一个参数必须是 view engine，第二个参数和app.engine这个方法中定义的模板引擎的名称（第一个参数）是一致的
 app.set('view engine', 'html');
 //在开发过程中，需要取消模板缓存
-swig.setDefaults({cache: false});
+swig.setDefaults({
+  cache: false
+});
 //bodyparser设置
-app.use( bodyParser.urlencoded({extended:false}) );
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 //设置跨域访问
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    next();
-    });
-
+//设置允许跨域访问该服务.
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  next();
+});
 // //设置cookie
 // app.use( function(req, res, next) {
 //     req.cookies = new Cookies(req, res);
@@ -63,12 +69,9 @@ app.all('*', function(req, res, next) {
 // } );
 
 /*
-* 根据不同的功能划分模块
-* */
+ * 根据不同的功能划分模块
+ * */
 //案例展示
 app.use('/admin', require('./routes/admin'));
 app.use('/user', require('./routes/user'));
-
-// app.use('/api', require('./routers/api'));
-// app.use('/', require('./routes/user'));
- app.listen(8888);
+app.listen(8888);
--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var db = require('../conf/conf.js');
-var formaDate = require('../utils/date.js');
-var multer = require('multer');
+var db = require("../conf/conf.js");
+var formaDate = require("../utils/date.js");
+var multer = require("multer");
 //统一返回格式
 var responseData;
 router.use(function (req, res, next) {
   responseData = {
     code: 0,
-    message: ''
-  }
+    message: ""
+  };
   next();
 });
 // 字段说明
 //  isShow：0 表示展示      1 表示物理删除即隐藏
 
 //首页数据
-router.post('/baoming', function (req, res) {
+router.post("/baoming", function (req, res) {
   let studentName = req.body.studentName;
   let phone = req.body.phone;
   let wantCountry = req.body.wantCountry;
@@ -26,250 +26,240 @@ router.post('/baoming', function (req, res) {
   let major = req.body.major;
   let xueli = req.body.xueli;
   let isShow = 0;
-  let sql = 'insert  into  reportList(studentName,phone,wantCountry,wantSchool,QQ,email,major,xueli,isShow) values(?,?,?,?,?,?,?,?,?)'
-  var params = [studentName, phone, wantCountry, wantSchool, QQ, email, major, xueli, isShow];
+  let sql =
+    "insert  into  reportList(studentName,phone,wantCountry,wantSchool,QQ,email,major,xueli,isShow) values(?,?,?,?,?,?,?,?,?)";
+  var params = [
+    studentName,
+    phone,
+    wantCountry,
+    wantSchool,
+    QQ,
+    email,
+    major,
+    xueli,
+    isShow
+  ];
   db.query(sql, params, function (err, results) {
-    if (err) { } else {
+    if (err) {} else {
       res.json({
-        msg: '操作成功',
-        status: '200',
-      })
+        msg: "操作成功",
+        status: "200"
+      });
     }
   });
 });
 
-
 //留言表单
-router.post('/liuyan', function (req, res) {
+router.post("/liuyan", function (req, res) {
   let username = req.body.username;
   let phone = req.body.phone;
   let QQ = req.body.QQ;
   let email = req.body.email;
   let content = req.body.content;
   let isShow = 0;
-  let sql = 'insert  into  MessageBoard(username,phone,QQ,email,content,isShow) values(?,?,?,?,?,?)'
+  let sql =
+    "insert  into  MessageBoard(username,phone,QQ,email,content,isShow) values(?,?,?,?,?,?)";
   var param = [username, phone, QQ, email, content, isShow];
   db.query(sql, param, function (err, results) {
-    if (err) { } else {
+    if (err) {} else {
       res.json({
-        msg: '操作成功',
-        status: '200',
-      })
+        msg: "操作成功",
+        status: "200"
+      });
     }
   });
 });
 // 留言列表
-router.post('/liuyan/list', function (req, res) {
+router.post("/liuyan/list", function (req, res) {
   let sql = "SELECT * FROM MessageBoard where isShow=0 and status=1";
   db.query(sql, function (err, results) {
     if (err) {
-      console.log(err)
+      console.log(err);
     } else {
       res.json({
-        msg: '操作成功',
-        status: '200',
+        msg: "操作成功",
+        status: "200",
         data: results
-      })
+      });
     }
   });
 });
 
 // 公司简介
-router.post('/companyprofile', function (req, res) {
+router.post("/companyprofile", function (req, res) {
   let type = req.query.type;
   let isShow = 0;
   let sql = `SELECT * FROM Companyprofile where isShow=0 and type=${type}`;
   db.query(sql, function (err, results) {
-    if (err) { } else {
+    if (err) {} else {
       res.json({
-        msg: '操作成功',
-        status: '200',
+        msg: "操作成功",
+        status: "200",
         data: results
-      })
+      });
     }
   });
 });
 
 // 头部导航国家的获取
-router.post('/getcountry', function (req, res) {
+router.post("/getcountry", function (req, res) {
   let sql = `SELECT * FROM countryconfig   where isShow=0  group by  typeid`;
   db.query(sql, function (err, results) {
     if (err) {
       res.json({
-        msg: '失败',
-        status: '0',
+        msg: "失败",
+        status: "0",
         msg: err
-      })
+      });
     } else {
       let arr = [];
       let arrStr = "";
       var getData1 = new Promise(function (resolve, reject) {
         results.map((item, index) => {
-          let sql = `SELECT * FROM countryconfig   where isShow=0  and  typeid='${item.typeid}'`;
+          let sql = `SELECT * FROM countryconfig   where isShow=0  and  typeid='${
+            item.typeid
+          }'`;
           db.query(sql, function (err, respon) {
             if (err) {
               reject(err);
-            } else {             
-              arrStr = { typeid: item.typeid, countrylist: respon };
+            } else {
+              arrStr = {
+                typeid: item.typeid,
+                countrylist: respon
+              };
               arr.push(arrStr);
-              if(index==(results.length-1)){
-                resolve(arr)
+              if (index == results.length - 1) {
+                resolve(arr);
               }
-              
             }
-          })
-        })
-      })
+          });
+        });
+      });
       getData1.then(function (respon) {
         res.json({
-          msg: '成功',
+          msg: "成功",
           status: 200,
           data: respon
-        })
-      })
-
+        });
+      });
     }
   });
 });
-
-
-
 // 首页学校信息
-router.post('/indexSchool', function (req, res) {
+router.post("/indexSchool", function (req, res) {
   let isShow = 0;
   let sql = `SELECT * FROM  famousSchools   where isShow=0  group by  country`;
   db.query(sql, function (err, results) {
     if (err) {
       res.json({
-        msg: '失败',
-        status: '0',
+        msg: "失败",
+        status: "0",
         msg: err
-      })
+      });
     } else {
       let arr = [];
       let arrStr = "";
       var getData1 = new Promise(function (resolve, reject) {
         results.map((item, index) => {
-          let sql = `SELECT * FROM famousSchools   where isShow=0  and  country='${item.country}'`;
+          let sql = `SELECT * FROM famousSchools   where isShow=0  and  country='${
+            item.country
+          }'`;
           db.query(sql, function (err, respon) {
             if (err) {
               reject(err);
-            } else {             
-              arrStr = { country: item.country,amount:item.amount, schoolList: respon };
+            } else {
+              arrStr = {
+                country: item.country,
+                amount: item.amount,
+                schoolList: respon
+              };
               arr.push(arrStr);
-              if(index==(results.length-1)){
-                resolve(arr)
+              if (index == results.length - 1) {
+                resolve(arr);
               }
-              
             }
-          })
-        })
-      })
+          });
+        });
+      });
       getData1.then(function (respon) {
         res.json({
-          msg: '成功',
+          msg: "成功",
           status: 200,
           data: respon
-        })
-      })
-
+        });
+      });
     }
   });
 });
 
 // 获取首页的留学信息
-router.post('/liuxueInformation', function (req, res) {
+router.post("/liuxueInformation", function (req, res) {
   let isShow = 0;
   let sql = `SELECT * FROM  news   where isShow=0  group by  country`;
   db.query(sql, function (err, results) {
     if (err) {
       res.json({
-        msg: '失败',
-        status: '0',
+        msg: "失败",
+        status: "0",
         msg: err
-      })
+      });
     } else {
       let arr = [];
       let arrStr = "";
       var getData1 = new Promise(function (resolve, reject) {
         results.map((item, index) => {
-          let sql = `SELECT * FROM  news   where isShow=0  and  country='${item.country}'`;
+          let sql = `SELECT * FROM  news   where isShow=0  and  country='${
+            item.country
+          }'`;
           db.query(sql, function (err, respon) {
             if (err) {
               reject(err);
-            } else {             
-              arrStr = { country: item.country,newsList: respon };
+            } else {
+              arrStr = {
+                country: item.country,
+                newsList: respon
+              };
               arr.push(arrStr);
-              if(index==(results.length-1)){
-                resolve(arr)
+              if (index == results.length - 1) {
+                resolve(arr);
               }
-              
             }
-          })
-        })
-      })
+          });
+        });
+      });
       getData1.then(function (respon) {
         res.json({
-          msg: '成功',
+          msg: "成功",
           status: 200,
           data: respon
-        })
-      })
-
+        });
+      });
     }
   });
 });
 
-
-
 // 拿到国家的轮播图
-
-router.post('/banner', function (req, res) {
+router.post("/banner", function (req, res) {
   let id = req.query.id;
   let isShow = 0;
   let sql = `SELECT * FROM countryconfig where isShow=0 and Id=${id}`;
   db.query(sql, function (err, results) {
-    if (err) { } else {
+    if (err) {} else {
       res.json({
-        msg: '操作成功',
-        status: '200',
+        msg: "操作成功",
+        status: "200",
         data: results
-      })
+      });
     }
   });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //获取当前时间
 function formatDate() {
   //把时间戳转化为日期对象
-  let date = new Date()
+  let date = new Date();
   //调用封装，参数为日期对象和时间格式
-  return formaDate.formaDate(date, 'yyyy-MM-dd hh:mm')
+  return formaDate.formaDate(date, "yyyy-MM-dd hh:mm");
 }
 //------------------------------图片上传------------------------------------------
 //获取时间
@@ -284,10 +274,11 @@ function getNowFormatDate() {
   if (strDate >= 0 && strDate <= 9) {
     strDate = "0" + strDate;
   }
-  var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate;
+  var currentdate =
+    date.getFullYear() + seperator1 + month + seperator1 + strDate;
   return currentdate.toString();
 }
-var datatime = 'public/images/' + getNowFormatDate();
+var datatime = "public/images/" + getNowFormatDate();
 //将图片放到服务器
 var storage = multer.diskStorage({
   // 如果你提供的 destination 是一个函数，你需要负责创建文件夹
@@ -300,10 +291,10 @@ var storage = multer.diskStorage({
 var upload = multer({
   storage: storage
 });
-router.post('/upload', upload.single('picUrl'), function (req, res) {
+router.post("/upload", upload.single("picUrl"), function (req, res) {
   res.json({
     state: 200,
     ret_code: req.file.path
   });
-})
+});
 module.exports = router;
