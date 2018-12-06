af63cb0cfc64bd36eba8e778451b18ab9d44eefb
yeyifu <1437552152@qq.com>
2018-12-07 00:14:06 +0800

    tijiao

--- routes/admin.js
+++ routes/admin.js
@@ -14,7 +14,6 @@ router.use(function (req, res, next) {
 });
 // 字段说明
 //  isShow：0 表示展示      1 表示物理删除即隐藏
-
 //----------------------------------------案例开始------------------
 // 获得案例
 router.post("/team", function (req, res) {
@@ -907,6 +906,139 @@ router.post("/school/detail", function (req, res) {
   });
 });
 //------------------------------------------------------ 价格套餐结束  ------------------------------------------------------------
+
+
+
+//------------------------------- 该国留学的优势--------------------------------------
+// 获得套餐
+router.post("/priceList", function (req, res) {
+  let allCount;
+  let pageNo = parseInt(req.body.pageNo);
+  let pageSize = parseInt(req.body.pageSize);
+  let sql = "SELECT COUNT(*) FROM pricemeal where isShow=0";
+  let sql2 =
+    "SELECT*FROM pricemeal where isShow=0 limit" +
+    " " +
+    (pageNo - 1) * pageSize +
+    "," +
+    pageNo * pageSize;
+  db.query(sql, function (err, results) {
+    if (err) {} else {
+      allCount = results[0]["COUNT(*)"];
+      back(allCount);
+    }
+  });
+
+  function back(allCount) {
+    db.query(sql2, function (err, results) {
+      if (err) {
+        res.json({
+          msg: err,
+          status: "0"
+        });
+      } else {
+        var allPage = allCount / pageSize;
+        var pageStr = allPage.toString();
+        // 不能整除
+        if (pageStr.indexOf(".") > 0) {
+          allPage = parseInt(pageStr.split(".")[0]) + 1;
+        }
+        res.json({
+          msg: "操作成功",
+          status: "200",
+          totalPages: allPage,
+          data: results,
+          total: allCount,
+          currentPage: parseInt(pageNo)
+        });
+      }
+    });
+  }
+});
+
+//删除一个套餐
+router.post("/price/delete", function (req, res) {
+  let id = req.body.Id;
+  let sql = "UPDATE pricemeal  set isShow=? where Id=?";
+  let param = ["1", id];
+  db.query(sql, param, function (err, results) {
+    if (err) {} else {
+      res.json({
+        msg: "操作成功",
+        status: "200"
+      });
+    }
+  });
+});
+//增加一个套餐
+router.post("/price/add", function (req, res) {
+  let des = req.body.des;
+  let country = req.body.country;
+  let price = req.body.price;
+  let addtime = req.body.addtime;
+  let address = req.body.address;
+  let isShow = 0;
+  let sql = "insert  into  pricemeal(des,country,price,addtime,address,isShow) values(?,?,?,?,?,?)";
+  var param = [des, country, price, addtime, address, isShow];
+  db.query(sql, param, function (err, results) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.json({
+        msg: "操作成功",
+        status: "200"
+      });
+    }
+  });
+});
+// 修改一个套餐
+router.post("/price/update", function (req, res) {
+  let des = req.body.des;
+  let country = req.body.country;
+  let price = req.body.price;
+  let addtime = req.body.addtime;
+  let address = req.body.address;
+  let Id = req.body.Id;
+  let sql = "UPDATE pricemeal SET des=?,country=?,price=?,addtime=?,address=? where Id=?";
+  var param = [des, country, price, addtime, address, Id];
+  db.query(sql, param, function (err, results) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.json({
+        msg: "操作成功",
+        status: "200"
+      });
+    }
+  });
+});
+
+//一个价格套餐详情
+router.post("/school/detail", function (req, res) {
+  let id = req.body.id;
+  let sql = "SELECT * FROM pricemeal where Id=" + id;
+  db.query(sql, function (err, results) {
+    if (err) {} else {
+      res.json({
+        msg: "操作成功",
+        status: "200",
+        data: results
+      });
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 //获取当前时间
 function formatDate() {
